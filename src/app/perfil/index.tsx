8e2732caa1b294c893c03d6328d14886198b2daa
KaueCMP <kaue.s.campos@aluno.senai.br>
2026-08-09 12:34:27 -0300

    add perfil page structure

--- src/app/perfil/index.tsx
+++ src/app/perfil/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native"
-
+import { Text, TouchableOpacity, View } from "react-native"
+import { Image } from "expo-image";
 const Perfil = () => {
     return (
         <View>
             <Text>Perfil</Text>
+            <View>
+                <View>
+                    <Image source={require('imagem do perfil')} />
+                    <View>
+                        <Text>Késsia milena</Text>
+                        <Text>kessia@email.com</Text>
+                    </View>
+                </View>
+            </View>
+            <TouchableOpacity>
+                <Text>Sair da Conta</Text>
+            </TouchableOpacity>
         </View>
     )
 }
