8e12fe4d648abfc576016ef268aa7cd92ceebf05
KaueCMP <kaue.s.campos@aluno.senai.br>
2026-08-09 12:34:05 -0300

    add criar os page structure

---
+++ src/app/criarOs/index.tsx
@@ -0,0 +1,36 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native"
+
+const index = () => {
+    return (
+        <View>
+            <Text>Criar ordem de serviço</Text>
+            <View>
+                <View>
+                    <Text>Título do problema *</Text>
+                    <TextInput placeholder="Ex: Vazamento da pia" />
+                </View>
+                <View>
+                    <Text>Máquina / Equipamento *</Text>
+                    <TextInput placeholder="Ex: Cadeira ergonômica" />
+                </View>
+                <View>
+                    <Text>Local / Setor *</Text>
+                    <TextInput placeholder="Ex: Sala 7 - 1° Andar" />
+                </View>
+                <View>
+                    <Text>Descrição do problema *</Text>
+                    <TextInput placeholder="Ex: O botão que aumenta o nivel da cadeira não está funcionando." />
+                </View>
+                <View>
+                    <Text>Imagem / Foto do problema *</Text>
+                    <TextInput placeholder="insira a imagem" />
+                </View>
+                <TouchableOpacity>
+                    <Text>Abrir Odem de Serviço</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default index
